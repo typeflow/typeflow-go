d499de18ed5341d52a1f5eb55c6159b929543aeb
Alessandro Diaferia <alediaferia@gmail.com>
2015-06-20 19:44:05 +0100

    Implement incremental matrix-based levenshtein distance calculation

--- golevenshtein.go
+++ golevenshtein.go
@@ -6,10 +6,6 @@ import (
 	"math"
 )
 
-type Score int
-
-
-
 func minimum(values ...int) (int, error) {
 	if len(values) == 0 {
 		return -1, errors.New("Cannot find minimum of empty list")
@@ -27,9 +23,9 @@ func minimum(values ...int) (int, error) {
  * A slow recursive levenshtein implementation
  * only used for benchmarking purposes
  */
-func compareSlicesRecursive(first, second []byte) Score {
+func compareSlicesRecursive(first, second []byte) int {
 	if len(first) == 0 || len(second) == 0 {
-		return Score(math.Max(float64(len(first)), float64(len(second))))
+		return  int(math.Max(float64(len(first)), float64(len(second))))
 	}
 	len1 := len(first)
 	len2 := len(second)
@@ -47,5 +43,156 @@ func compareSlicesRecursive(first, second []byte) Score {
 		fmt.Println(err)
 		return -1
 	}
-	return Score(min)
+	return min
 }
+
+type LState struct {
+	matrix   [][]int
+
+	// w1 = cols, w2 = rows
+	w1       []rune
+	w2       []rune
+}
+
+func InitLState() (ls *LState) {
+	ls = new(LState)
+	ls.matrix = nil
+
+	return
+}
+
+func (state *LState) UpdateState(w1part, w2part []rune) {
+	if len(w1part) == 0 || len(w2part) == 0 {
+		panic("Unexpected 0 length argument")
+	}
+    if state.matrix == nil {
+        state.initializeMatrix(len(w1part), len(w2part))
+		state.w1 = w1part
+		state.w2 = w2part
+
+		state.fillMatrix(0, 0)
+	} else {
+		incr1 := len(state.w1)
+		incr2 := len(state.w2)
+
+		// we need to increase size
+		state.w1 = append(state.w1, w1part...)
+		state.w2 = append(state.w2, w2part...)
+
+		cols := make([][]int, len(state.w1) + 1)
+		copy(cols, state.matrix)
+		for i := 0; i < len(cols); i++ {
+			col := make([]int, len(state.w2) + 1)
+			if i < len(state.matrix) {
+				copy(col, state.matrix[i])
+			}
+			cols[i] = col
+		}
+
+		state.matrix = cols
+
+		// initializing the extended part now
+		for i := incr1; i < len(state.w1); i++ {
+			if cap(state.matrix[i]) > i {
+				state.matrix[i][0] = i
+			} else {
+				state.matrix[i] = append(state.matrix[i], i)
+			}
+		}
+
+		// initializing
+		for i := incr2; i < len(state.w2); i++ {
+			if cap(state.matrix[0]) > i {
+				state.matrix[0][i] = i
+			} else {
+				state.matrix[0] = append(state.matrix[0], i)
+			}
+		}
+
+		state.fillMatrix(incr2 - 1, incr1 - 1)
+	}
+}
+
+func (state *LState) initializeMatrix(s1, s2 int) {
+	state.matrix = make([][]int, s1 + 1)
+	for i := 0; i < len(state.matrix); i++ {
+		state.matrix[i] = make([]int, s2 + 1)
+	}
+
+	// initializing
+	for i := 0; i < s1; i++ {
+		state.matrix[i][0] = i
+	}
+
+	// initializing
+	for i := 0; i < s2; i++ {
+		state.matrix[0][i] = i
+	}
+}
+
+func (state *LState) fillMatrix(startRow, startCol int) {
+	for j := startRow; j < len(state.w2); j++ {
+		for i := startCol; i < len(state.w1); i++ {
+			if state.w1[i] == state.w2[j] {
+				state.matrix[i+1][j+1] = state.matrix[i][j]
+			} else {
+				v, err := minimum(state.matrix[i][j+1] + 1,
+					state.matrix[i + 1][j] + 1,
+					state.matrix[i][j] + 1)
+				if err != nil {
+					panic(err)
+				}
+
+				state.matrix[i+1][j+1] = v
+			}
+		}
+	}
+}
+
+func (state *LState) Distance() int {
+	if state.matrix != nil {
+		return state.matrix[len(state.w1)][len(state.w2)]
+	}
+	return math.MaxInt32
+}
+
+/*
+func matrixBasedLevenshtein(w1, w2 []rune) int {
+    // making the matrix
+	// that will hold our results
+
+	matrix := make([][]int, len(w1) + 1)
+	for i := 0; i < len(matrix); i++ {
+		matrix[i] = make([]int, len(w2) + 1)
+	}
+
+	// initializing
+	for i := 0; i < len(w1); i++ {
+		matrix[i][0] = i
+	}
+
+	// initializing
+	for i := 0; i < len(w2); i++ {
+		matrix[0][i] = i
+	}
+
+	for j := 0; j < len(w2); j++ {
+		for i := 0; i < len(w1); i++ {
+			if w1[i] == w2[j] {
+				matrix[i+1][j+1] = matrix[i][j]
+			} else {
+				v, err := minimum(matrix[i][j+1] + 1,
+				                       matrix[i + 1][j] + 1,
+				                       matrix[i][j] + 1)
+				if err != nil {
+					panic(err)
+				}
+
+				matrix[i+1][j+1] = v
+			}
+		}
+	}
+
+	return matrix[len(w1)][len(w2)]
+}
+*/
--- golevenshtein_test.go
+++ golevenshtein_test.go
@@ -4,15 +4,24 @@ import (
 	"testing"
 )
 
-var testCases = []struct {
+type perm struct {
 	source      string
 	destination string
-	distance    Score
-}{
+	distance    int
+}
+
+var testCases = []perm{
 	{"alessandro", "lessandro", 1},
 	{"alessandro", "alesasndro", 2},
 	{"zzz", "az", 2},
 	{"--|", "---", 1},
+}
+
+var incrementalTestCases = [][]perm{
+	{ {"aaa", "abc", 2 } },
+	{{ "al", "al",  0 }, { "ex", "es", 1}},
+	{{"a", "b", 1}, {"aa", "ba", 2}},
+	{{"a", "a", 0}, { "l", "l", 0}, {"e", "e", 0}, {"s", "s", 0}, {"s", "a", 1}, {"a", "s", 2}, {"n", "n", 2} },
 }
 
 func Test_compareSlicesRecursive(t *testing.T) {
@@ -23,5 +32,26 @@ func Test_compareSlicesRecursive(t *testing.T) {
 	}
 }
 
+func Test_compareSlicesWithMatrixBaseCase(t *testing.T) {
+	for _, v := range incrementalTestCases[0] {
+		ls := InitLState()
+		ls.UpdateState([]rune(v.source), []rune(v.destination))
 
+		if cmp := ls.Distance(); cmp != v.distance {
+			t.Errorf("Comparing '%s' and '%s' failed: result was %v but expected was %v", v.source, v.destination, cmp, v.distance)
+		}
+	}
+}
 
+func Test_compareSlicesWithMatrix(t* testing.T) {
+	for _, v := range incrementalTestCases {
+		ls := InitLState()
+		for _, increment := range v {
+			ls.UpdateState([]rune(increment.source), []rune(increment.destination))
+
+			if cmp := ls.Distance(); cmp != increment.distance {
+				t.Errorf("Comparing '%s' and '%s' failed: result was %v but expected was %v", increment.source, increment.destination, cmp, increment.distance)
+			}
+		}
+	}
+}
