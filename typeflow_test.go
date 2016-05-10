a0f3375db6a3734165bc25f6019bdec885812f26
Alessandro Diaferia <alediaferia@gmail.com>
2016-05-10 11:41:53 +0100

    Move to prefixmap

--- typeflow_test.go
+++ typeflow_test.go
@@ -1,166 +1,166 @@
 package typeflow
 
 import (
-	"testing"
-	"fmt"
+    "testing"
+    "fmt"
 )
 
 func (s* LState) print_matrix(t* testing.T) {
-	rows := ""
-	for i := 0; i < len(s.matrix[0]); i++ {
-		row := ""
-		for j := 0; j < len(s.matrix); j++ {
-			row = fmt.Sprintf("%s[ %v ]", row, s.matrix[j][i])
-		}
-		rows = fmt.Sprintf("%s%s\n", rows, row)
-	}
-	t.Logf("\n%s\n", rows)
+    rows := ""
+    for i := 0; i < len(s.matrix[0]); i++ {
+        row := ""
+        for j := 0; j < len(s.matrix); j++ {
+            row = fmt.Sprintf("%s[ %v ]", row, s.matrix[j][i])
+        }
+        rows = fmt.Sprintf("%s%s\n", rows, row)
+    }
+    t.Logf("\n%s\n", rows)
 }
 
 type permutation_t struct {
-	source      string
-	destination string
-	distance    int
+    source      string
+    destination string
+    distance    int
 }
 
 var basic_test_cases = []permutation_t{
-	{"alessandro", "lessandro", 1},
-	{"alessandro", "alesasndro", 2},
-	{"zzz", "az", 2},
-	{"--|", "---", 1},
+    {"alessandro", "lessandro", 1},
+    {"alessandro", "alesasndro", 2},
+    {"zzz", "az", 2},
+    {"--|", "---", 1},
 }
 
 func TestLevenshteinDistance(t *testing.T) {
-	for _, v := range basic_test_cases {
-		if cmp := LevenshteinDistance(v.source, v.destination); cmp != v.distance {
-			t.Errorf("Comparing '%s' and '%s' failed: result was %v but expected was %v",
-				v.source,
-				v.destination,
-				cmp,
-				v.distance)
-		}
-	}
+    for _, v := range basic_test_cases {
+        if cmp := LevenshteinDistance(v.source, v.destination); cmp != v.distance {
+            t.Errorf("Comparing '%s' and '%s' failed: result was %v but expected was %v",
+                v.source,
+                v.destination,
+                cmp,
+                v.distance)
+        }
+    }
 }
 
 // represents a single increment
 // within a test case with its
 // associated distance value
 type increment_t struct {
-	source   string
-	distance int
+    source   string
+    distance int
 }
 
 // represents a generic
 // incremental test case
 type incremental_test_case_t struct {
-	target    			string
-	source_increments []increment_t
+    target                string
+    source_increments []increment_t
 }
 
 var incremental_test_cases = []incremental_test_case_t{
-	{"abc", []increment_t{{"aaa", 2}}},
-	{ "alex", []increment_t{{"al",  2}, { "es", 1}}}, // alex, ales
-	{ "aaale", []increment_t{{"b", 5}, {"ba", 4}, {"a", 4}}}, // aaale, bbaa
-	{"iraq", []increment_t{{"rep of ireland", 11}}},
+    {"abc", []increment_t{{"aaa", 2}}},
+    { "alex", []increment_t{{"al",  2}, { "es", 1}}}, // alex, ales
+    { "aaale", []increment_t{{"b", 5}, {"ba", 4}, {"a", 4}}}, // aaale, bbaa
+    {"iraq", []increment_t{{"rep of ireland", 11}}},
 }
 
 func Test_compare_silces_r(t *testing.T) {
-	for _, v := range incremental_test_cases {
-		var source []rune
-		for _, increment := range v.source_increments {
-			source = append(source, []rune(increment.source)...)
+    for _, v := range incremental_test_cases {
+        var source []rune
+        for _, increment := range v.source_increments {
+            source = append(source, []rune(increment.source)...)
 
-			if cmp := compare_silces_r([]rune(source), []rune(v.target)); cmp != increment.distance {
-				t.Errorf("Comparing '%s' and '%s' failed: result was %v but expected was %v",
-					source,
-					v.target,
-					cmp,
-					increment.distance)
-			}
-		}
-	}
+            if cmp := compare_silces_r([]rune(source), []rune(v.target)); cmp != increment.distance {
+                t.Errorf("Comparing '%s' and '%s' failed: result was %v but expected was %v",
+                    source,
+                    v.target,
+                    cmp,
+                    increment.distance)
+            }
+        }
+    }
 }
 
 func TestDistanceBaseCase(t *testing.T) {
-	for _, v := range incremental_test_cases {
-		var source   []rune
-		var target   []rune = []rune(v.target)
-		for _, tc := range v.source_increments {
-			source = append(source, []rune(tc.source)...)
-		}
+    for _, v := range incremental_test_cases {
+        var source   []rune
+        var target   []rune = []rune(v.target)
+        for _, tc := range v.source_increments {
+            source = append(source, []rune(tc.source)...)
+        }
 
-		ls := InitLState(string(target))
-		ls.UpdateState([]rune(source))
+        ls := InitLState(string(target))
+        ls.UpdateState([]rune(source))
 
-		if cmp := ls.Distance(); cmp != v.source_increments[len(v.source_increments)-1].distance {
-			t.Logf("Comparing '%s' and '%s' failed: result was %v but expected was %v\n",
-				string(source),
-				string(target), cmp, v.source_increments[len(v.source_increments)-1].distance)
-			t.Logf("Matrix was:\n")
-			ls.print_matrix(t)
-			t.FailNow()
-		}
-	}
+        if cmp := ls.Distance(); cmp != v.source_increments[len(v.source_increments)-1].distance {
+            t.Logf("Comparing '%s' and '%s' failed: result was %v but expected was %v\n",
+                string(source),
+                string(target), cmp, v.source_increments[len(v.source_increments)-1].distance)
+            t.Logf("Matrix was:\n")
+            ls.print_matrix(t)
+            t.FailNow()
+        }
+    }
 }
 
 func TestDistanceIncremental(t* testing.T) {
-	for _, v := range incremental_test_cases {
-		ls := InitLState(v.target)
+    for _, v := range incremental_test_cases {
+        ls := InitLState(v.target)
 
-		for _, increment := range v.source_increments {
-			ls.UpdateState([]rune(increment.source))
+        for _, increment := range v.source_increments {
+            ls.UpdateState([]rune(increment.source))
 
-			if cmp := ls.Distance(); cmp != increment.distance {
-				t.Logf("Comparing '%s' and '%s' incrementally failed: result was %v but expected was %v\n",
-					string(ls.source),
-					string(ls.target),
-					cmp,
-					increment.distance)
-				t.Logf("Matrix was:\n")
-				ls.print_matrix(t)
-				t.FailNow()
-			}
-		}
+            if cmp := ls.Distance(); cmp != increment.distance {
+                t.Logf("Comparing '%s' and '%s' incrementally failed: result was %v but expected was %v\n",
+                    string(ls.source),
+                    string(ls.target),
+                    cmp,
+                    increment.distance)
+                t.Logf("Matrix was:\n")
+                ls.print_matrix(t)
+                t.FailNow()
+            }
+        }
 
-		if len(v.source_increments) > 1 { // test allows for roll back
-			err := ls.RollbackBy(len(v.source_increments[len(v.source_increments) - 1].source))
-			if err != nil {
-				t.Errorf("An error occurred during rollback: %v", err)
-			}
+        if len(v.source_increments) > 1 { // test allows for roll back
+            err := ls.RollbackBy(len(v.source_increments[len(v.source_increments) - 1].source))
+            if err != nil {
+                t.Errorf("An error occurred during rollback: %v", err)
+            }
 
-			if newDistance := ls.Distance(); newDistance != v.source_increments[len(v.source_increments) - 2].distance {
-				t.Errorf("Distance after rolling back is %d, expected is %d",
-					newDistance,
-					v.source_increments[len(v.source_increments) - 2].distance)
-			}
-		}
-	}
+            if newDistance := ls.Distance(); newDistance != v.source_increments[len(v.source_increments) - 2].distance {
+                t.Errorf("Distance after rolling back is %d, expected is %d",
+                    newDistance,
+                    v.source_increments[len(v.source_increments) - 2].distance)
+            }
+        }
+    }
 }
 
 type similarity_range struct {
-	low  float32
-	high float32
+    low  float32
+    high float32
 }
 
 type expected_match struct {
-	word string
-	similarity_range similarity_range
+    string string
+    similarity_range similarity_range
 }
 
 func Benchmark_recursiveLevenshtein(b *testing.B) {
     for i := 0; i < b.N; i++ {
-    	for _, v := range basic_test_cases {
-    		compare_silces_r([]rune(v.source), []rune(v.destination))
-    	}
+        for _, v := range basic_test_cases {
+            compare_silces_r([]rune(v.source), []rune(v.destination))
+        }
     }
 }
 
 func Benchmark_matrixLevenshtein(b *testing.B) {
     for i:= 0; i < b.N; i++ {    
-    	for _, v := range basic_test_cases {
-    		ls := InitLState(v.destination)
-    		ls.UpdateState([]rune(v.source))
-    	}
+        for _, v := range basic_test_cases {
+            ls := InitLState(v.destination)
+            ls.UpdateState([]rune(v.source))
+        }
     }
 }
 
--- word_source.go
+++ word_source.go
@@ -1,140 +1,104 @@
 package typeflow
 
 import (
-	. "github.com/typeflow/triego"
+    "github.com/alediaferia/prefixmap"
+    "strings"
+    "regexp"
 )
 
-// Represents a filter to apply to the
-// source setted via the SetSource method.
-// A filter will be passed the currently processed
-// word and will return either a modified version
-// for that word or true for the skip param which will
-// make the WordSource ignore the current word.
-type WordFilter func(w string) (word string, skip bool)
-
-// The actual WordSource type which
-// will hold the source you set through
-// SetSource
 type WordSource struct {
-	trie *Trie
-	wc   int
+    prefixMap *prefixmap.PrefixMap
+    wc   int
+}
+
+const allowedKeyChars = `[^a-zA-Z0-9]+`
+var allowedKeyCharsRegexp *regexp.Regexp
+
+func init() {
+    allowedKeyCharsRegexp = regexp.MustCompile(allowedKeyChars)
 }
 
 // Represents a match
 // found
 type Match struct {
-	// The actual word which matched
-	// the requested string
-	Word string `json:"word"`
+    // The actual string which matched
+    // the requested string
+    String string `json:"string"`
 
-	// The similarity value for the
-	// current word.
-	// Similarity is 1 when
-	// the two words are equal.
-	// See github.com/typeflow/typeflow-go for
-	// more details on how the similarity is computed.
-	Similarity float32 `json:"similarity"`
+    // The similarity value for the
+    // current word.
+    // Similarity is 1 when
+    // the two words are equal.
+    // See github.com/typeflow/typeflow-go for
+    // more details on how the similarity is computed.
+    Similarity float32 `json:"similarity"`
 }
 
 func computeSimilarity(lenw1, lenw2, ld int) float32 {
-	den := maximum(lenw1, lenw2)
+    den := maximum(lenw1, lenw2)
 
-	return 1.0 - float32(ld)/float32(den)
+    return 1.0 - float32(ld)/float32(den)
 }
 
-// Initializes a new empty WordSource
+// NewWordSource Initializes a new empty WordSource
 func NewWordSource() (ws *WordSource) {
-	ws = new(WordSource)
-	ws.trie = NewTrie()
-	ws.wc = 0
-	return
+    ws = new(WordSource)
+    ws.prefixMap = prefixmap.New()
+    ws.wc = 0
+    return
 }
 
-// Sets the given strings slice as the
+// SetSource sets the given strings slice as the
 // current source after applying the given
 // filters in order
-func (ws *WordSource) SetSource(words []string, filters []WordFilter) {
-
-	for _, w := range words {
-		for _, filter := range filters {
-			w, skip := filter(w)
-			if !skip {
-				ws.trie.AppendWord(w)
-				ws.wc += 1
-			}
-		}
-	}
+func (ws *WordSource) SetSource(strs []string) {    
+    for _, s := range strs {
+        keys := allowedKeyCharsRegexp.Split(strings.TrimSpace(s), -1)
+        for _, k := range keys {
+            ws.prefixMap.Insert(k, s)
+        }
+    }
 }
 
 type dirty_range struct {
-	low    int
-	length int
+    low    int
+    length int
 }
 
 type Matches []Match
 
 func (matches Matches) Len() int {
-	return len(matches)
+    return len(matches)
 }
 
 func (matches Matches) Less(i, j int) bool {
-	return matches[i].Similarity > matches[j].Similarity
+    return matches[i].Similarity > matches[j].Similarity
 }
 
 func (matches Matches) Swap(i, j int) {
-	mi := matches[i]
-	mj := matches[j]
-	matches[i] = mj
-	matches[j] = mi
+    mi := matches[i]
+    mj := matches[j]
+    matches[i] = mj
+    matches[j] = mi
 }
 
-// Finds a match among the current words
+// FindMatch Finds a match among the current words
 // in the source.
 // minSimilarity is the minimum accepted similarity
 // to use when filling the matches slice.
 // Param substr is the string to match against.
 func (ws *WordSource) FindMatch(substr string, minSimilarity float32) (matches []Match, err error) {
-	matches = make([]Match, 0, ws.wc)
-	err = nil
-
-	ws.trie.EachPrefix(func(info PrefixInfo) (skipsubtree, halt bool) {
-
-		// if this prefix
-		// is not yet a word we took
-		// the advantage of preparing
-		// the comparison matrix
-		// in advance as for sure the
-		// words that are about to come
-		// will have this as prefix string
-		if !info.IsWord {
-			return false, false
-		}
-
-		// a flag to remember if we found
-		// matches or not at this round
-		var found_matches bool = false
-
-		// for each of the matching strings
-		for _, value := range info.Data {
-			similarity := computeSimilarity(len(value), len(substr), LevenshteinDistance(value, substr))
-			if info.Prefix == substr {
-				similarity += 1.0
-			}
-
-			if similarity >= minSimilarity {
-				matches = append(matches, Match{value, similarity})
-				found_matches = true
-			}
-		}
-		if found_matches {
-			return false, false
-		}
-
-		// the computed similarity is too low
-		// so there's no need to proceed further
-		// with this subtree
-		return true, false
-	})
-
-	return
+    matches = make([]Match, 0, ws.wc)
+    err = nil
+    
+    values := ws.prefixMap.GetByPrefix(substr)
+    for _, v := range values {
+        value := v.(string)
+        similarity := computeSimilarity(len(value), len(substr), LevenshteinDistance(value, substr))
+        if similarity >= minSimilarity {
+            matches = append(matches, Match{value, similarity})
+        }
+    }
+    
+    return matches, err
 }
